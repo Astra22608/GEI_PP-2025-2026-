83980e6a5b0b229f9c0ce2857aacf6de7d54cc2c
Candela Naya López <candelacnl@gmail.com>
2025-12-16 21:54:24 +0100

    Refactor robot movement and pathfinding logic

--- 10/shortest.ml
+++ 10/shortest.ml
@@ -1,66 +1,52 @@
-type move = North | South | West | East 
+type move = North | South | West | East
 
-let move_position (row, col) direction =
-  match direction with
-  | North -> (row - 1, col)
-  | South -> (row + 1, col)
-  | East -> (row, col + 1)
-  | West -> (row, col - 1)
+let delta = function
+  | North -> (-1, 0)
+  | South -> (1, 0)
+  | East  -> (0, 1)
+  | West  -> (0, -1)
 
-let is_valid_position (row, col) m n =
-  row >= 1 && row <= m && col >= 1 && col <= n
+let opposite = function
+  | North -> South | South -> North
+  | East  -> West  | West  -> East
 
-let move_robot (row, col) direction m n =
-  let (new_row, new_col) = move_position (row, col) direction in
-  if is_valid_position (new_row, new_col) m n then
-    ((new_row, new_col), direction)
-  else
-    let new_direction = match direction with
-      | North -> South
-      | South -> North
-      | East -> West
-      | West -> East
-    in
-    ((row, col), new_direction)
+let next_pos (r, c) mv =
+  let dr, dc = delta mv in
+  (r + dr, c + dc)
 
-let step_robots robots m n =
-  List.map (fun (pos, dir) -> move_robot pos dir m n) robots
+let move_robot m n (pos, dir) =
+  let new_pos = next_pos pos dir in
+  if fst new_pos >= 1 && fst new_pos <= m &&
+     snd new_pos >= 1 && snd new_pos <= n
+  then (new_pos, dir)
+  else (next_pos pos (opposite dir), opposite dir)
 
-let robot_positions robots =
-  List.map fst robots
+let shortest m n init_robots =
+  let goal = (m, n) in
+  let moves = [North; South; East; West] in
 
-let has_robot pos robots =
-  List.mem pos (robot_positions robots)
+  let q = Queue.create () in
+  let visited = Hashtbl.create 20000 in
 
-let shortest m n robots =
-  let rec bfs queue visited =
-    match queue with
-    | [] -> raise Not_found
-    | (current_pos, current_robots, moves) :: rest ->
-        if current_pos = (m, n) then
-          List.rev moves
-        else
-          let state = (current_pos, current_robots) in
-          if List.mem state visited then
-            bfs rest visited
-          else
-            let new_visited = state :: visited in
-            let directions = [North; South; East; West] in
-            let next_states = List.filter_map (fun dir ->
-              let next_pos = move_position current_pos dir in
-              if is_valid_position next_pos m n then begin
-                let next_robots = step_robots current_robots m n in
-                if not (has_robot next_pos next_robots) then
-                  Some (next_pos, next_robots, dir :: moves)
-                else
-                  None
-              end else
-                None
-            ) directions in
-            bfs (rest @ next_states) new_visited
+  Queue.push ((1,1), init_robots, []) q;
+  Hashtbl.add visited ((1,1), init_robots) ();
+
+  let rec loop () =
+    let pos, robots, path = Queue.pop q in
+    if pos = goal then path
+    else
+      let next_robots = List.map (move_robot m n) robots in
+      List.iter (fun mv ->
+        let np = next_pos pos mv in
+        if fst np >= 1 && fst np <= m && snd np >= 1 && snd np <= n then
+          let occupied = List.exists (fun (rp,_) -> rp = np) next_robots in
+          if not occupied then
+            let state = (np, next_robots) in
+            if not (Hashtbl.mem visited state) then begin
+              Hashtbl.add visited state ();
+              Queue.push (np, next_robots, path @ [mv]) q
+            end
+      ) moves;
+      loop ()
   in
-  
-  if has_robot (1, 1) robots then
-    raise Not_found
-  else
-    bfs [((1, 1), robots, [])] []
+  loop ()
