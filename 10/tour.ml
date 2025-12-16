a8c608428272c8ca822cdb4500ce4911178d9bbf
Candela Naya López <candelacnl@gmail.com>
2025-12-16 21:54:00 +0100

    Update tour.ml

--- 10/tour.ml
+++ 10/tour.ml
@@ -1,74 +1,55 @@
-type move = North | South | West | East 
+type move = North | South | West | East
 
-let move_position (row, col) direction =
-  match direction with
-  | North -> (row - 1, col)
-  | South -> (row + 1, col)
-  | East -> (row, col + 1)
-  | West -> (row, col - 1)
+let delta = function
+  | North -> (-1, 0)
+  | South -> (1, 0)
+  | East  -> (0, 1)
+  | West  -> (0, -1)
 
-let is_valid_position (row, col) m n =
-  row >= 1 && row <= m && col >= 1 && col <= n
+let opposite = function
+  | North -> South | South -> North
+  | East  -> West  | West  -> East
 
-let move_robot (row, col) direction m n =
-  let (new_row, new_col) = move_position (row, col) direction in
-  if is_valid_position (new_row, new_col) m n then
-    ((new_row, new_col), direction)
+let add (r, c) mv =
+  let dr, dc = delta mv in
+  (r + dr, c + dc)
+
+let move_robot m n (pos, dir) =
+  let new_pos = add pos dir in
+  let nr, nc = new_pos in
+  if 1 <= nr && nr <= m && 1 <= nc && nc <= n then
+    (new_pos, dir)
   else
-    let new_direction = match direction with
-      | North -> South
-      | South -> North
-      | East -> West
-      | West -> East
-    in
-    ((row, col), new_direction)
+    (add pos (opposite dir), opposite dir)
 
-let step_robots robots m n =
-  List.map (fun (pos, dir) -> move_robot pos dir m n) robots
+let occupied pos robots =
+  List.exists (fun (p, _) -> p = pos) robots
 
-let robot_positions robots =
-  List.map fst robots
+let tour m n initial_robots =
+  let goal = (m, n) in
+  let moves = [North; South; East; West] in
 
-let has_robot pos robots =
-  List.mem pos (robot_positions robots)
-
-let tour m n robots =
-  let visited = ref [] in
-  
-  let rec dfs current_pos current_robots moves =
-    if current_pos = (m, n) then
-      Some (List.rev moves)
+  let rec search pos visited robots path =
+    if pos = goal then
+      List.rev path
     else
-      let state = (current_pos, current_robots) in
-      if List.mem state !visited then
-        None
-      else begin
-        visited := state :: !visited;
-        
-        let directions = [North; South; East; West] in
-        let rec try_direction = function
-          | [] -> None
-          | dir :: rest ->
-              let next_pos = move_position current_pos dir in
-              if is_valid_position next_pos m n then begin
-                let next_robots = step_robots current_robots m n in
-                if not (has_robot next_pos next_robots) then
-                  match dfs next_pos next_robots (dir :: moves) with
-                  | Some result -> Some result
-                  | None -> try_direction rest
-                else
-                  try_direction rest
-              end else
-                try_direction rest
-        in
-        try_direction directions
-      end
+      let rec try_moves = function
+        | [] -> raise Not_found
+        | mv :: rest ->
+            let next_pos = add pos mv in
+            let r, c = next_pos in
+            if r < 1 || r > m || c < 1 || c > n || List.mem next_pos visited then
+              try_moves rest
+            else
+              let next_robots = List.map (move_robot m n) robots in
+              if occupied next_pos next_robots then
+                try_moves rest
+              else
+                (try
+                   search next_pos (next_pos :: visited) next_robots (mv :: path)
+                 with Not_found ->
+                   try_moves rest)
+      in
+      try_moves moves
   in
-  
-  let initial_robots = robots in
-  if has_robot (1, 1) initial_robots then
-    raise Not_found
-  else
-    match dfs (1, 1) initial_robots [] with
-    | Some result -> result
-    | None -> raise Not_found
+  search (1,1) [(1,1)] initial_robots []
